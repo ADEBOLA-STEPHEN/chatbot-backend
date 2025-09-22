cae49f9e918c5eecd20de3cbdff9675146b73490
Ojifinni Adebola Stephen <adebolaojifinni@gmail.com>
2025-09-22 17:22:43 +0100

    changes to confidence threshold

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def generate_response(user_input):
             tag = model.classes_[max_idx]
 
             # Apply confidence threshold
-            if confidence < 0.35:
+            if confidence < 0.49:
                 responses.append("I’m not sure I understand 🤔")
                 continue
         except Exception:
